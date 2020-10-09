d36d8f55bb00f632d551ff2207761e85c5a0705c
Robbe Van Herck <robbe@robbevanherck.be>
2020-10-09 15:50:42 +0200

    Implement edit location form

--- app/controllers/locations/location/edit.js
+++ app/controllers/locations/location/edit.js
@@ -1,9 +1,34 @@
 import { action } from "@ember/object";
 import Controller from "@ember/controller";
+import {task} from "ember-concurrency";
 
 export default class LocationsLocationEditController extends Controller {
+  locationAddressValidation = {}; //TODO
+
   @action
-  async setAddress(address) {
-    this.address = address;
+  goBack() {
+    history.back();
   }
+
+  @task(
+    /**
+     * @param {BufferedChangeset} changeSet
+     * @this {ItemsNewController}
+     */
+    function* (changeSet) {
+      // To get the address working in the same form, apply the changeset and save afterwards
+      // changeSet.execute();
+
+      // this.model.location.address = yield this.model.address.save();
+
+      // yield this.model.location.save();
+
+      changeSet.execute();
+      this.model.location.save();
+      this.model.address.save();
+
+      this.goBack();
+    }
+  )
+  saveLocation;
 }
