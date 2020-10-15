a077eb679d57ee36f6968caf96fccd1ed12055b7
Robbe Van Herck <robbe@robbevanherck.be>
2020-10-15 21:41:56 +0200

    Implement reservation saving

--- app/controllers/initiatives/initiative/reservation/edit.js
+++ app/controllers/initiatives/initiative/reservation/edit.js
@@ -1,50 +1,54 @@
-import {action} from "@ember/object";
-import {tracked} from "@glimmer/tracking";
 import Controller from "@ember/controller";
 import {task} from "ember-concurrency";
+import {action} from "@ember/object";
 /**
  * @typedef {import("ember-changeset/types").BufferedChangeset} BufferedChangeset
  */
 
 
-export default class ReservationEditController extends Controller {
+export default class InitiativesInitiativeReservationEditController extends Controller {
   newReservationValidation = {}; //TODO
+
+  @action
+  goBack() {
+    history.back();
+  }
+
 
   @task(
     /**
      * @param {BufferedChangeset} changeSet
-     * @this {ReservationEditController}
+     * @this {InitiativesInitiativeReservationEditController}
      */
     function* (changeSet) {
-      // TODO: this needs to be rewritten to account for reservations time/date
-      /*const items = yield this.store.findAll('item');
-      items.forEach((item) => {
-        const quantity = changeSet.get(item.id);
-        item.reservations.then((existingReservations) => {
-          if (existingReservations && existingReservations.length >= 1) {
-            existingReservations.forEach((targetReservation) => {
-              console.log(typeof quantity);
-              if (!quantity) {
-                console.log("Deleting reservations");
-                return targetReservation.deleteRecord().save();
-              } else if (quantity !== targetReservation.quantity) {
-                console.log("Updating quantity");
-                targetReservation.quantity = quantity;
-                targetReservation.save();
-              }
-            });
-          } else {
-            // We don't want to add empty reservations, so a truthy check is ok here
-            if (quantity) {
-              console.log("Creating new");
-              return this.store.createRecord('reservations', {
-                quantity,
-                item: item,
-              }).save();
-            }
-          }
+      const items = yield this.store.query('item', {'filter[:id:]' : changeSet.changes.map(c => c.key).join(',')});
+      yield Promise.all(items.map(async (item) => {
+        const reservations = await this.store.query('reservation',  {
+          'filter[initiative][:id:]': this.model.initiative.get('id'),
+          'filter[item][:id:]': item.get('id'),
         });
-      });*/
+        const quantity = parseInt(changeSet.get(item.id));
+        if (quantity === 0 && reservations.length > 0) {
+          await Promise.all(reservations.map(async (reservation) => {
+            await reservation.destroyRecord();
+          }));
+        } else if(quantity !== 0) {
+          console.log(reservations.length);
+          if (reservations.length > 0) {
+            await Promise.all(reservations.map((reservation) => {
+              reservation.quantity = quantity;
+              return reservation.save();
+            }))
+          } else {
+            await this.store.createRecord('reservation', {
+              initiative: this.model.initiative,
+              item,
+              quantity,
+            }).save();
+          }
+        }
+      }));
+      this.goBack();
     }
   )
   addReservation;
